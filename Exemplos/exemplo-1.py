433d4af3a67da97c4aac0c34452723262502c37b
Daniel Saad <daniel.saad.nunes@gmail.com>
2016-10-17 15:40:15 -0200

    Adicionando exemplos e arquivos

---
+++ Exemplos/exemplo-1.py
@@ -0,0 +1,25 @@
+"""
+    Autor: Daniel Saad Nogueira Nunes
+    Comentários: Exemplo minimalista da plotagem da função $f(x) = x$ utilizando
+    a biblioteca Matplotlib
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#   cria um objeto figura e o coloca em fig
+fig = plt.figure()
+#   da figura criada, é alocada uma matriz 1x1 de gráficos
+#   ax corresponde a matriz 1
+ax = fig.add_subplot(111);
+#   gera o dominio da função y = x
+#   domínio da função [0,10] com amostras de tamanho 0.1
+x  = np.arange(0,10,0.1)
+#   Gera a imagem da função y = x
+y = [ a for a in x ]
+#   Plota a imagem
+l = plt.plot(x,y)
+#   Coloca um título correspondente na figura
+t = ax.set_title("Gráfico da Função " r"$f(x) = x$")
+#   Mostra o resultado na tela
+plt.show()
---
+++ Exemplos/exemplo-2.py
@@ -0,0 +1,29 @@
+"""
+    Autor: Daniel Saad Nogueira Nunes
+    Comentários: Exemplo da plotagem da função $f(x) = x$ utilizando
+    a biblioteca Matplotlib. Neste caso são adicionadas informações nos
+    eixos x e y do gráfico.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#   Cria um objeto figura e o coloca em fig
+fig = plt.figure()
+#   Da figura criada, é alocada uma matriz 1x1 de gráficos
+#   ax corresponde a matriz 1
+ax = fig.add_subplot(111);
+#   Gera o dominio da função y = x
+x  = np.arange(0,10,0.1) # domínio da função [0,10] com amostras de tamanho 0.1
+#   Gera a imagem da função y = x
+y = [ a for a in x ]
+#   Plota a imagem
+l = plt.plot(x,y)
+#   Coloca um título correspondente na figura
+t = ax.set_title("Gráfico da Função " r"$f(x) = x$")
+#   Insere título no eixo x
+t_x = ax.set_xlabel("Domínio da função")
+#   Insere título no eixo y
+t_y = ax.set_ylabel("Imagem da Função")
+#   Mostra o resultado na tela
+plt.show()
